60c2520b1c320c2228b5f118b10319800bdcc9f9
Thirat <100107926+Thiratpyz@users.noreply.github.com>
2024-06-26 21:43:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,24 @@
 import streamlit as st
 import cv2
-from ultralytics import YOLO
-from PIL import Image
-import numpy as np
 
-# Load the YOLOv8 model
-model = YOLO("yolov8m-pose.pt")
+def main():
+    st.set_page_config(page_title="Streamlit WebCam App")
+    st.title("Webcam Display Steamlit App")
+    st.caption("Powered by OpenCV, Streamlit")
+    cap = cv2.VideoCapture(0)
+    frame_placeholder = st.empty()
+    stop_button_pressed = st.button("Stop")
+    while cap.isOpened() and not stop_button_pressed:
+        ret, frame = cap.read()
+        if not ret:
+            st.write("Video Capture Ended")
+            break
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_placeholder.image(frame,channels="RGB")
+        if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
 
-st.title("Real-time YOLOv8 Object Detection")
-st.text("Using YOLOv8 model with Streamlit")
-
-# Function to process each frame
-def process_frame(frame):
-    results = model(frame, conf=0.3)
-    annotated_frame = results[0].plot()  # Get the annotated frame
-    return annotated_frame
-
-# Start video capture
-cap = cv2.VideoCapture(0)
-
-stframe = st.empty()
-
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        st.write("Failed to capture video")
-        break
-
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    processed_frame = process_frame(frame)
-
-    stframe.image(processed_frame, channels="RGB")
-
-cap.release()
+if __name__ == "__main__":
+    main()
